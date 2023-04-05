e7433fe4fd480f8285b22a8b12dd83094b4c0fa9
Anmolpreet Kaur <anmolpreetkaur547@gmail.com>
2023-04-05 11:50:28 -0600

    Made an object for questions in main.js -wrapped h1 tags in a section with id

--- fill_in_the _blank/main.js
+++ fill_in_the _blank/main.js
@@ -1,31 +1,38 @@
-const idtQuestions=[
-{
-    'question': "is the medical term for High Blood Pressure?",
-    'answer':"hypertension",
-},{
-    'question': " is medical term for itch caused by mites?",
-    'answer':"scabies",
-},
+const idtQuestions = [
+    {
+        'question': "is the medical term for High Blood Pressure?",
+        'answer': "hypertension",
+    }, {
+        'question': " is medical term for itch caused by mites?",
+        'answer': "scabies",
+    },
+    {
+        'question': " is medical term for itch caused by mites?",
+        'answer': "scabies",
+    }
 ]
-
-const ques = document.getElementsByTagName('h1');
-console.log(ques);
-
-for( i=0; i<idtQuestions.length; i++){
-ques[i].innerHTML=`${idtQuestions[i].question}`;
-}
-
-
-
 
 window.addEventListener("DOMContentLoaded", (event) => {
     document.getElementById("idt-submit").addEventListener("click", clickButton);
+    const fitb=document.getElementById("idt-fill-in-the-blank");
+    const ques = fitb.getElementsByTagName('h1');
+    console.log(ques);
+    if (ques.length !== idtQuestions.length) {
+
+        console.error("Arrays are not the same length")
+    } else {
+        for (i = 0; i < ques.length; i++) {
+            ques[i].innerHTML += `${idtQuestions[i].question}`;
+        }
+    }
+
+
 });
 
 function clickButton() {
     console.log("Clicked a Button");
     const answer = document.getElementById("idt-q1");
-    const rightAnswer= document.getElementById("idt-op1");
+    const rightAnswer = document.getElementById("idt-op1");
 
     console.log(answer.value);
     if (answer.value.toLowerCase().trim() === "hypertension") {
