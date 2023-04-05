6a2d9a8a2cdf2e555ca89ce114eaa2fea3eb5d36
Anmolpreet Kaur <anmolpreetkaur547@gmail.com>
2023-04-05 09:43:34 -0600

    Pairing and got q1 working -changed the ids to include idt- -.value() to get value out of textarea -.trim() to get rid of spaces from the end of strings -used .toLowerCase() to make comparison easier

--- fill_in_the _blank/main.js
+++ fill_in_the _blank/main.js
@@ -1,5 +1,22 @@
-document.getElementById("op5").addEventListener("click", clickButton);
+window.addEventListener("DOMContentLoaded", (event) => {
 
+    document.getElementById("idt-submit").addEventListener("click", clickButton);
+});
 function clickButton() {
-  console.log("Clicked a Button")
+    console.log("Clicked a Button");
+    const answer = document.getElementById("idt-q1");
+    console.log(answer.value);
+    if (answer.value.toLowerCase().trim() === "hypertension") {
+
+        console.log("Answer is correct")
+
+
+
+    }
+    else {
+        console.log("Answer is incorrect")
+
+    }
 }
+
+
