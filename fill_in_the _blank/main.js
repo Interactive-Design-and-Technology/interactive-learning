bad0a8f69569d2c3a576de491e53a19f99c1701d
Anmolpreet Kaur <anmolpreetkaur547@gmail.com>
2023-04-05 11:18:43 -0600

    WIP Trying to make an object for questions

--- fill_in_the _blank/main.js
+++ fill_in_the _blank/main.js
@@ -1,13 +1,31 @@
-window.addEventListener("DOMContentLoaded", (event) => {
+const idtQuestions=[
+{
+    'question': "is the medical term for High Blood Pressure?",
+    'answer':"hypertension",
+},{
+    'question': " is medical term for itch caused by mites?",
+    'answer':"scabies",
+},
+]
 
+const ques = document.getElementsByTagName('h1');
+console.log(ques);
+
+for( i=0; i<idtQuestions.length; i++){
+ques[i].innerHTML=`${idtQuestions[i].question}`;
+}
+
+
+
+
+window.addEventListener("DOMContentLoaded", (event) => {
     document.getElementById("idt-submit").addEventListener("click", clickButton);
 });
+
 function clickButton() {
     console.log("Clicked a Button");
     const answer = document.getElementById("idt-q1");
     const rightAnswer= document.getElementById("idt-op1");
-   
-
 
     console.log(answer.value);
     if (answer.value.toLowerCase().trim() === "hypertension") {
