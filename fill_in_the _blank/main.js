663abe9400218c0475b6878615df0903753f661e
Anmolpreet Kaur <anmolpreetkaur547@gmail.com>
2023-04-05 08:20:31 -0600

    -Added a function with an event listener to console log a message -Not working apparently

--- fill_in_the _blank/main.js
+++ fill_in_the _blank/main.js
@@ -1,11 +1,5 @@
-$(".showInTable").click(function(){
-    $(this).hide();
-    $(this).next("aside").show();
-});
+document.getElementById("op5").addEventListener("click", clickButton);
 
-// Reset button functions. Clears feedback and brings back the "check answers" buttons.
-$(".reset").click(function(){
-    $(".cf-table textarea").val('');
-    $(".showInTable").show();
-    $(".cf-table aside").hide();
-});
+function clickButton() {
+  console.log("Clicked a Button")
+}
