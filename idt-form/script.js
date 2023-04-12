508d0f5d510aa4f58a18c0596023109e026c051c
Raunaq Singh <Raunaqs398@gmail.com>
2023-04-12 12:17:57 -0600

    WIP started over
    - the code from chatGTP was a good start but it didn't really have what was needed for the form so I am starting over
    - this is just basic code with no styling
    - right now alot is broken inside the code that still needs to be fixed

--- idt-form/script.js
+++ idt-form/script.js
@@ -1,55 +1,15 @@
-function generateJSON() {
-    // Create the HTML form elements
-    const form = document.createElement("form");
-  
-    const nameLabel = document.createElement("label");
-    nameLabel.textContent = "Name:";
-    const nameInput = document.createElement("input");
-    nameInput.type = "text";
-    nameInput.id = "name";
-    nameInput.name = "name";
-  
-    const ageLabel = document.createElement("label");
-    ageLabel.textContent = "Age:";
-    const ageInput = document.createElement("input");
-    ageInput.type = "number";
-    ageInput.id = "age";
-    ageInput.name = "age";
-  
-    const button = document.createElement("button");
-    button.type = "button";
-    button.textContent = "Generate JSON";
-    button.addEventListener("click", function() {
-      // Get the values from the form
-      const name = document.getElementById("name").value;
-      const age = document.getElementById("age").value;
-  
-      // Create a JavaScript object with the form values
-      const person = {
-        "name": name,
-        "age": age
-      };
-  
-      // Convert the object to a JSON string
-      const json = JSON.stringify(person);
-  
-      // Download the JSON file
-      const downloadLink = document.createElement("a");
-      downloadLink.download = "person.json";
-      downloadLink.href = "data:text/json;charset=utf-8," + encodeURIComponent(json);
-      downloadLink.click();
-    });
-  
-    // Add the form elements to the form
-    form.appendChild(nameLabel);
-    form.appendChild(nameInput);
-    form.appendChild(document.createElement("br"));
-    form.appendChild(ageLabel);
-    form.appendChild(ageInput);
-    form.appendChild(document.createElement("br"));
-    form.appendChild(button);
-  
-    // Add the form to the page
-    document.body.appendChild(form);
-  }
-  
+window.addEventListener("DOMContentLoaded", (event) => {
+  console.log("DOM fully loaded and parsed");
+
+  let idtAddBtn = document.getElementById('idt-add-btn')
+  let idtMain = document.getElementById('idt-main')
+  idtAddBtn.addEventListener("click", (event) => {
+    let i = 0
+    idtMain.innerHTML += `<label for="question">Q${i}:</label>
+    <input type="text" id="idt-question-${i}" name="question-${i}">
+    <label for="answer">A${i}:</label>
+    <input type="text" id="idt-answer-${i}" name="answer-${i}">`
+
+    i++
+  })
+});
