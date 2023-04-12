c1d284892e5907fcd339aa14569aa9572457e7d7
Raunaq Singh <Raunaqs398@gmail.com>
2023-04-12 14:32:16 -0600

    WIP the code just generates q and a input boxes rn
    - my plan for the future is to store the q and a in an object

--- idt-form/script.js
+++ idt-form/script.js
@@ -1,16 +1,36 @@
 window.addEventListener("DOMContentLoaded", (event) => {
   console.log("DOM fully loaded and parsed");
 
-  let idtAddBtn = document.getElementById('idt-add-btn')
-  let idtMain = document.getElementById('idt-main')
+  let idtAddBtn = document.getElementById('idt-add-btn');
+
+  let idtMain = document.getElementById('idt-QAs');
+
   idtAddBtn.addEventListener("click", (event) => {
-
-    idtMain.innerHTML += `<br><div><label for="question">Q:</label>
-    <input type="text" id="idt-question" name="question">
+    idtMain.innerHTML += `<br>
+    <label for="question">Q:
+    <input type="text" name="question">
+    </label>
     <br>
-    <label for="answer">A:</label>
-    <input type="text" id="idt-answer" name="answer"></div><br>`
+    <label for="answer">A:
+    <input type="text" name="answer">
+    </label>
+    <br>`
 
-    
-  })
+    console.log(idtMain.getElementsByTagName("input"))
+  });
+ 
+  let idtGenBtn = document.getElementById('idt-generate');
+  let genCode = document.getElementById("idt-generated-code");
+
+  idtGenBtn.addEventListener("click", (event)=> {
+    // this helps grab all the information from the inputs before we add it to the code snipit 
+   let qaInput = idtMain.getElementsByTagName("input")
+
+  //  loop through all the inputs to get the value and store them somewhere 
+  
+    for(i=0; i < qaInput.length; i = i + 2){
+      
+    }
+  });
+ 
 });
