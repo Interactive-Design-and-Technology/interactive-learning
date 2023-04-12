0170fc2ecc2c34eddd52374d2e84a687cc551941
Raunaq Singh <Raunaqs398@gmail.com>
2023-04-12 12:26:14 -0600

    WIP making a prototype of how the site would look there is no functionality at the moment
    - fixed the issue where only one input would be shown
    
    **Need to fix**
    - multiple id's with the same name
    - add a bit of styling

--- idt-form/script.js
+++ idt-form/script.js
@@ -4,12 +4,13 @@ window.addEventListener("DOMContentLoaded", (event) => {
   let idtAddBtn = document.getElementById('idt-add-btn')
   let idtMain = document.getElementById('idt-main')
   idtAddBtn.addEventListener("click", (event) => {
-    let i = 0
-    idtMain.innerHTML += `<label for="question">Q${i}:</label>
-    <input type="text" id="idt-question-${i}" name="question-${i}">
-    <label for="answer">A${i}:</label>
-    <input type="text" id="idt-answer-${i}" name="answer-${i}">`
 
-    i++
+    idtMain.innerHTML += `<br><div><label for="question">Q:</label>
+    <input type="text" id="idt-question" name="question">
+    <br>
+    <label for="answer">A:</label>
+    <input type="text" id="idt-answer" name="answer"></div><br>`
+
+    
   })
 });
