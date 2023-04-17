cd800e2f6d0de5cc96b6ab1bde98cb97825f1962
Arlin Schaffel <arlin.schaffel@saskpolytech.ca>
2023-04-17 15:28:31 -0600

    Add Prism for Code Highlighting
    - this will allow the code that is generated to be colored appropriately
    - also added a copy to clipboard button
    - read more about prism.js here: https://prismjs.com/

--- docs/script.js
+++ docs/script.js
@@ -2,7 +2,6 @@ window.addEventListener("DOMContentLoaded", (event) => {
   console.log("DOM fully loaded and parsed");
 
   let idtAddBtn = document.getElementById('add-question');
-
   let idtMain = document.getElementById('question-answer');
 
   idtAddBtn.addEventListener("click", (event) => {
@@ -39,18 +38,22 @@ window.addEventListener("DOMContentLoaded", (event) => {
     }
 
     let codeInfo = ``
-
     console.log(questionArray)
 
     for (i = 0; i < questionArray.length; i++) {
-      codeInfo += `
-     <p>{
-      'question' : '${questionArray[i].question}',
-     'answer': '${questionArray[i].answer}'
-    },</p>
-    `
+      codeInfo += `  {'question' : '${questionArray[i].question}','answer': '${questionArray[i].answer}'},\n`
     }
-    genCode.innerHTML = `[${codeInfo}]`
+    genCode.innerHTML = `[\n${codeInfo}]`
   });
-
 });
+
+function copyToClipboard() {
+  const code = document.getElementById('code').innerText;
+  navigator.clipboard.writeText(code)
+    .then(() => {
+      console.log('Code copied to clipboard successfully!');
+    })
+    .catch(err => {
+      console.error('Failed to copy code: ', err);
+    });
+}
