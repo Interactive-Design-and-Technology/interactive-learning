41ac48774b450e9ee98b91c73b91246c1bcc8c21
Arlin Schaffel <arlin.schaffel@saskpolytech.ca>
2023-04-19 10:03:47 -0600

    fill_in_the_blank refactor
    - similar to last few commits, make running script more consistent
    - removed image to make more generic

--- fill_in_the _blank/main.js
+++ fill_in_the _blank/main.js
@@ -1,34 +1,15 @@
-const idtQuestions = [
-    {
-        'question': "is the medical term for High Blood Pressure?",
-        'answer': "hypertension",
-    }, {
-        'question': " is medical term for itch caused by mites?",
-        'answer': "scabies",
-    },
-    {
-        'question': " percentage of the human body is water?",
-        'answer': "66%",
-    },
-    {
-        'question': " is the number of taste buds are on your tongue",
-        'answer': "Nine Thousand",
-    },
-    {
-        'question': " is the largest organ of the human body",
-        'answer': "Skin",
-    },
-    {
-        'question': "  part of the human body is effected in Astigmatism",
-        'answer': "Eyes",
-    },
-    {
-        'question': " fat-soluble substance present in green leafy vegetables was given its name because it is required for coagulation of the blood to take place?",
-        'answer': "Vitamin K",
-    },
-]
 
-window.addEventListener("DOMContentLoaded", (event) => {
+function idtFillBlank(idtQuestions) {
+
+    const main_container = document.getElementById("idt-fill-in-the-blank");
+    main_container.innerHTML = `
+        <h1>Type all the answers in the blanks and click submit button to see if your answers are wrong or right </h1>
+        <ol id="idt-questions"></ol>
+        <div id="idt-options"></div>
+        <div>
+            <button id="idt-submit">Click to Submit Answers</button>
+        </div>`;
+
     document.getElementById("idt-submit").addEventListener("click", clickButton);
 
     const question_list = document.getElementById('idt-questions');
@@ -42,37 +23,37 @@ window.addEventListener("DOMContentLoaded", (event) => {
     for (let i = 0; i < idtQuestions.length; i++) {
         options_list.innerHTML += `<div>${idtQuestions[i].answer}</div>`;
     }
-});
 
-function clickButton() {
-    // loop through all of the questions and buttons
-    // if the question matches the button, then add the class "correct"
-    // if the question does not match the button, then add the class "incorrect"
-    const question_list = document.getElementById('idt-questions');
-    const options_list = document.getElementById('idt-options');
+    function clickButton() {
+        // loop through all of the questions and buttons
+        // if the question matches the button, then add the class "correct"
+        // if the question does not match the button, then add the class "incorrect"
+        const question_list = document.getElementById('idt-questions');
+        const options_list = document.getElementById('idt-options');
 
-    // if questions_list and options_list don't have the same number of children
-    // then we have a problem and should throw an error
-    if (question_list.children.length != options_list.children.length) {
-        throw new Error("The number of questions and options do not match.");
-    }
+        // if questions_list and options_list don't have the same number of children
+        // then we have a problem and should throw an error
+        if (question_list.children.length != options_list.children.length) {
+            throw new Error("The number of questions and options do not match.");
+        }
 
-    // loop through all of the questions and options
-    for (let i = 0; i < idtQuestions.length; i++) {
-        console.log('hello?');
-        // user_answer is the text that the user typed in - <li><textarea>.value
-        const user_answer = question_list.children[i].children[0].value;
+        // loop through all of the questions and options
+        for (let i = 0; i < idtQuestions.length; i++) {
+            console.log('hello?');
+            // user_answer is the text that the user typed in - <li><textarea>.value
+            const user_answer = question_list.children[i].children[0].value;
 
-        // options is the button with corresponding answer - <button>
-        const option = options_list.children[i];
+            // options is the button with corresponding answer - <button>
+            const option = options_list.children[i];
 
-        // if the user_answer matches the answer, then add the class "correct"
-        if (idtQuestions[i].answer.toLowerCase().trim() === user_answer.toLowerCase().trim()) {
-            option.classList.remove("incorrect");
-            option.classList.add("correct");
-        } else { // otherwise, add the class "incorrect"
-            option.classList.remove("correct");
-            option.classList.add("incorrect");
+            // if the user_answer matches the answer, then add the class "correct"
+            if (idtQuestions[i].answer.toLowerCase().trim() === user_answer.toLowerCase().trim()) {
+                option.classList.remove("incorrect");
+                option.classList.add("correct");
+            } else { // otherwise, add the class "incorrect"
+                option.classList.remove("correct");
+                option.classList.add("incorrect");
+            }
         }
     }
 }
