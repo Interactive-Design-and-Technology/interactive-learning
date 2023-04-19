34ae0420154d7d1ace00908caefa522f26c08c0f
Raunaq Singh <Raunaqs398@gmail.com>
2023-04-19 11:16:01 -0600

    Changes:
    - added a h1
    - made the code similar to the code in the cleanup branch
    - made a function for the javascript to live in
    - changed the div in html to a section

--- expandable-accordion/script.js
+++ expandable-accordion/script.js
@@ -1,23 +1,10 @@
-const questions = [
-    {
-        'question': 'Have you heard about the chocolate record player? ',
-        'answer': 'It sounds pretty sweet.'
-    },
-    {
-        'question': 'What do you call a factory that makes okay products?',
-        'answer': 'A satisfactory.'
-    },
-    {
-        'question': 'What did one wall say to the other?',
-        'answer': "I'll meet you at the corner."
-    }
-]
 
-window.addEventListener("DOMContentLoaded", (event) => {
 
-    let accordionId = document.getElementById("idt-accordion")
-
-    for(i = 0; i < questions.length; i++){
+function idtGenerate(questions) {
+    let accordionId = document.getElementById("idt-interactive-learning")
+    let acc = document.getElementsByClassName("accordion");
+    let i;
+    for (i = 0; i < questions.length; i++) {
         accordionId.innerHTML += `
         <button class="accordion">${questions[i].question}</button>
         <div class="panel">
@@ -25,23 +12,21 @@ window.addEventListener("DOMContentLoaded", (event) => {
         </div>`
     }
 
-    let acc = document.getElementsByClassName("accordion");
-    let i;
-    
+
     for (i = 0; i < acc.length; i++) {
-      acc[i].addEventListener("click", function() {
-        /* Toggle between adding and removing the "active" class,
-        to highlight the button that controls the panel */
-        this.classList.toggle("active");
-    
-        /* Toggle between hiding and showing the active panel */
-        let panel = this.nextElementSibling;
-        if (panel.style.display === "block") {
-          panel.style.display = "none";
-        } else {
-          panel.style.display = "block";
-        }
-      });
+        acc[i].addEventListener("click", function () {
+            /* Toggle between adding and removing the "active" class,
+            to highlight the button that controls the panel */
+            this.classList.toggle("active");
+
+            /* Toggle between hiding and showing the active panel */
+            let panel = this.nextElementSibling;
+            if (panel.style.display === "block") {
+                panel.style.display = "none";
+            } else {
+                panel.style.display = "block";
+            }
+        });
     }
 
-});
+};
