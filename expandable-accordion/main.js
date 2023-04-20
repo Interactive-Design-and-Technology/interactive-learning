9e1179ae513bc90a92264a23354c35d356dd048e
Arlin Schaffel <arlin.schaffel@saskpolytech.ca>
2023-04-20 09:12:21 -0600

    Cleanup
    - remove whitespace
    - change variable for readability

--- expandable-accordion/main.js
+++ expandable-accordion/main.js
@@ -1,10 +1,8 @@
-
-
 function idtGenerate(questions) {
     let accordionId = document.getElementById("idt-interactive-learning")
     let acc = document.getElementsByClassName("accordion");
-    let i;
-    for (i = 0; i < questions.length; i++) {
+
+    for (let i = 0; i < questions.length; i++) {
         accordionId.innerHTML += `
         <button class="accordion">${questions[i].question}</button>
         <div class="panel">
@@ -12,9 +10,8 @@ function idtGenerate(questions) {
         </div>`
     }
 
-
-    for (i = 0; i < acc.length; i++) {
-        acc[i].addEventListener("click", function () {
+    for (let j = 0; j < acc.length; j++) {
+        acc[j].addEventListener("click", function () {
             /* Toggle between adding and removing the "active" class,
             to highlight the button that controls the panel */
             this.classList.toggle("active");
@@ -28,5 +25,4 @@ function idtGenerate(questions) {
             }
         });
     }
-
 };
