81ffbcf0215d1e9cc6b042ac200e4f10d6f76386
Anmolpreet Kaur <anmolpreetkaur547@gmail.com>
2023-04-21 10:43:19 -0600

    WIP
    -Made some changes in div elements
    -Added an event listener to flip the card

---
+++ flash card/main.js
@@ -0,0 +1,7 @@
+window.addEventListener("DOMContentLoaded", (event) => {
+const card=document.getElementById("card-idt")
+card.addEventListener("click",flipcard);
+function flipcard(){
+    card.classList.toggle("flipCard");
+}
+});
