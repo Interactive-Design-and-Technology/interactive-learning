65e20120ec9ea45d53b4a1168d7123cf7fee3811
Anmolpreet Kaur <anmolpreetkaur547@gmail.com>
2023-04-24 08:59:40 -0600

    WIP
    -Trying to make the second card
    -Broke everything

--- flash card/main.js
+++ flash card/main.js
@@ -4,5 +4,10 @@ card.addEventListener("click",flipcard);
 function flipcard(){
     card.classList.toggle("flipCard");
 }
+const card_two=document.getElementById("card-idt-two")
+card_two.addEventListener("click",flipcard_two);
+function flipcard_two(){
+    card_two.classList.toggle("flipCard-two");
+}
 });
 
