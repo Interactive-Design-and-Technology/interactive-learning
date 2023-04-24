bf5d06bc0f3c730d0d3e539104c405f3d26e14f2
Anmolpreet Kaur <anmolpreetkaur547@gmail.com>
2023-04-24 14:02:26 -0600

    Successfully made four flipping cards
    -Generated data from JSON Object (I think)
    -Took out the unnecessary divs from HTML file

--- flash card/main.js
+++ flash card/main.js
@@ -3,19 +3,25 @@ const idtQuestions = [
       'question': "Who discovered X-rays?",
       'answer': "Wilhelm Röntgen",
     }, {
-      'question': " is medical term for itch caused by mites?",
-      'answer': "scabies",
+      'question': "Who is the only woman to have won two Nobel Prizes in two different fields?",
+      'answer': "Marie Curie",
     },
     {
-      'question': " percentage of the human body is water?",
-      'answer': "66%",
+      'question': "Which of the following technological developments came first?",
+      'answer': "Telescope",
     },
+    {
+        'question': "What airplane has not been flown commercially since 2003?",
+        'answer': "Concorde",
+      },
   ]
 window.addEventListener("DOMContentLoaded", (event) => {
 
     for (i = 0; i < idtQuestions.length; i++) {
-        document.getElementById('card-idt').innerHTML += `<div class="front"<p>${idtQuestions[i].question}</p></div>`
-        document.getElementById('card-idt').innerHTML += `<div class="back"<p>${idtQuestions[i].answer}</p></div>`
+        document.getElementById('card-idt').innerHTML += `<div class="front"<p>${idtQuestions[0].question}</p></div><div class="back"<p>${idtQuestions[0].answer}</p></div>`
+        document.getElementById('card-idt-two').innerHTML += `<div class="front"<p>${idtQuestions[1].question}</p></div><div class="back"<p>${idtQuestions[1].answer}</p></div>`
+        document.getElementById('card-idt-three').innerHTML += `<div class="front"<p>${idtQuestions[2].question}</p></div><div class="back"<p>${idtQuestions[2].answer}</p></div>`
+        document.getElementById('card-idt-four').innerHTML += `<div class="front"<p>${idtQuestions[3].question}</p></div><div class="back"<p>${idtQuestions[3].answer}</p></div>`
       }
     
 const card=document.getElementById("card-idt")
@@ -32,6 +38,11 @@ const card_three=document.getElementById("card-idt-three")
 card_three.addEventListener("click",flipcard_three);
 function flipcard_three(){
     card_three.classList.toggle("flipCard-three");
+}
+const card_four=document.getElementById("card-idt-four")
+card_four.addEventListener("click",flipcard_four);
+function flipcard_four(){
+    card_four.classList.toggle("flipCard-four");
 }
 });
 
