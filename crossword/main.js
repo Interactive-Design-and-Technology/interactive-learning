1d52621d4750713ed3c761d3aca5e3b488bf3377
Raunaq Singh <Raunaqs398@gmail.com>
2023-04-25 11:35:40 -0600

    WIP
    from yesterday's check in meeting I added some changes that were suggested such as maxlength

---
+++ crossword/main.js
@@ -0,0 +1,14 @@
+const questions = [
+    {
+        'question': 'Have you heard about the chocolate record player? ',
+        'answer': 'cat'
+    },
+    {
+        'question': 'What do you call a factory that makes okay products?',
+        'answer': 'tag'
+    },
+    {
+        'question': 'What did one wall say to the other?',
+        'answer': "bag"
+    }
+];
