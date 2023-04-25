7faa01b5260007de80c0993c1e61396a835d8d7c
Arlin Schaffel <arlin.schaffel@saskpolytech.ca>
2023-04-25 09:30:12 -0600

    WIP: refactoring flash-card
    - trying to generate everything and reduce duplication

--- flash-card/main.js
+++ flash-card/main.js
@@ -1,40 +1,45 @@
 
 function idtGenerate(idtQuestions) {
 
-      const main_container = document.getElementById("idt-interactive-learning");
-    main_container.innerHTML = `
-      <div class="card" id="card-idt">
-      </div>
-      <div class="card" id="card-idt-two">
-      </div>
-      <div class="card" id="card-idt-three">
-      </div>
-      <div class="card" id="card-idt-four">
-      </div>`;
+    const main_container = document.getElementById("idt-interactive-learning");
+    // main_container.innerHTML = `
+    //   <div class="card" id="card-idt">
+    //   </div>
+    //   <div class="card" id="card-idt-two">
+    //   </div>
+    //   <div class="card" id="card-idt-three">
+    //   </div>
+    //   <div class="card" id="card-idt-four">
+    //   </div>`;
 
   for (i = 0; i < idtQuestions.length; i++) {
-    document.getElementById('card-idt').innerHTML += `<div class="front"<p>${idtQuestions[0].question}</p></div><div class="back"<p>${idtQuestions[0].answer}</p></div>`
-    document.getElementById('card-idt-two').innerHTML += `<div class="front"<p>${idtQuestions[1].question}</p></div><div class="back"<p>${idtQuestions[1].answer}</p></div>`
-    document.getElementById('card-idt-three').innerHTML += `<div class="front"<p>${idtQuestions[2].question}</p></div><div class="back"<p>${idtQuestions[2].answer}</p></div>`
-    document.getElementById('card-idt-four').innerHTML += `<div class="front"<p>${idtQuestions[3].question}</p></div><div class="back"<p>${idtQuestions[3].answer}</p></div>`
+    main_container.innerHTML += `
+      <div class="card" id="card-idt-${i}">
+        <div class="front">
+          <p>${idtQuestions[i].question}</p>
+        </div>
+        <div class="back">
+          <p>${idtQuestions[i].answer}</p>
+        </div>
+      </div>`;
   }
 
-  const card = document.getElementById("card-idt")
+  const card = document.getElementById("card-idt-0")
   card.addEventListener("click", flipcard);
   function flipcard() {
     card.classList.toggle("flipCard");
   }
-  const card_two = document.getElementById("card-idt-two")
+  const card_two = document.getElementById("card-idt-1")
   card_two.addEventListener("click", flipcard_two);
   function flipcard_two() {
     card_two.classList.toggle("flipCard-two");
   }
-  const card_three = document.getElementById("card-idt-three")
+  const card_three = document.getElementById("card-idt-2")
   card_three.addEventListener("click", flipcard_three);
   function flipcard_three() {
     card_three.classList.toggle("flipCard-three");
   }
-  const card_four = document.getElementById("card-idt-four")
+  const card_four = document.getElementById("card-idt-3")
   card_four.addEventListener("click", flipcard_four);
   function flipcard_four() {
     card_four.classList.toggle("flipCard-four");
